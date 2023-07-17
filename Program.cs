d68fbc3ea6218ded67cf70c4294a4304e7dfcbc7
Kai Bellmann <kb@bellmann-engineering.com>
2023-07-17 10:27:31 +0200

    edited hello message

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using sample;
 
-Console.WriteLine(StringHelper.MakeUpperCase("Hello, World!"));
+Console.WriteLine(StringHelper.MakeUpperCase("Hello, schöne World!"));
